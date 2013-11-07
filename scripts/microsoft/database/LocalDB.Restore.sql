01611dcf430e17ab5b4f950927b2e53a08aa07f5
roxberry <roxberry@outlook.com>
2013-11-07 17:09:54 -0500

    Added script to restore db

---
+++ scripts/microsoft/database/LocalDB.Restore.sql
@@ -0,0 +1,9 @@
+﻿-- Use this for a LocalDB database
+-- Change paths as required
+RESTORE DATABASE eAds
+FROM DISK = 'D:\Workspaces\LocalDBs\eAds.bak'
+WITH MOVE 'eAds_Data' TO 'D:\Workspaces\LocalDBs\eAds_Data.MDF',
+MOVE 'eAds_Log' TO 'D:\Workspaces\LocalDBs\eAds_Log.LDF',
+REPLACE
+
+ALTER DATABASE eAds SET MULTI_USER
